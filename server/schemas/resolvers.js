81f4259404f7ed56bb3c2e05d7224a698efbafd2
kristermyr <110065764+kristermyr@users.noreply.github.com>
2023-04-01 15:30:37 -0600

    added .select to if context.user fixed the loading of saved books

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,12 +6,10 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       // check if users exist
-      console.log(context.user)
+      
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select(
-          "p_as_sw_ord"
-        );
-        console.log(userData)
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        
         return userData;
       }
       throw new AuthenticationError("Not logged in");
