1993882b52bc1d1e9f0271258cf12bb184d5130c
kristermyr <110065764+kristermyr@users.noreply.github.com>
2023-04-03 09:54:55 -0600

    deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/BookSearchEngine',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
